0333fc20bf67f149e9bd5128cde54f6578eb52f2
sowunmi mayowa <smayowa176@gmail.com>
2022-02-07 16:55:23 +0100

    created the navigation pages

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="About">
+            <h2 className="text-center text-2xl uppercase"><span className="text-blue-700 border-b-blue-700 font-bold">about</span> me </h2>
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div className="About">
+            <h2 className="text-center text-2xl uppercase"><span className="text-blue-700 border-b-blue-700 font-bold">contact</span> me </h2>
+        </div>
+    )
+}
+
+export default Contact
