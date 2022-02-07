030a1d199e6f4c26dde2e80296d2c6601e01c092
sowunmi mayowa <smayowa176@gmail.com>
2022-02-07 17:00:07 +0100

    imported routes for navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Home from './components/Home';
 import Navbar from './components/Navbar';
-
+import About from './components/pages/About';
+import {Routes, Route} from 'react-router-dom';
+import Contact from './components/pages/Contact';
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home/>
+
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/about" element={ <About /> } />
+          <Route path="/contact" element={ <Contact /> } />
+        </Routes>
     </div>
   );
 }
